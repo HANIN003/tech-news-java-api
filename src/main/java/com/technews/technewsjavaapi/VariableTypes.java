8472966fe85b240fc8e4297d45d18a329a817b5e
Nate Hanisch <nathan.hanisch86@gmail.com>
2023-04-01 19:39:29 -0400

    creating demo models

---
+++ src/main/java/com/technews/technewsjavaapi/VariableTypes.java
@@ -0,0 +1,7 @@
+package com.technews.technewsjavaapi;
+
+public class VariableTypes {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/technews/technewsjavaapi/VariablesPractice.java
@@ -0,0 +1,17 @@
+package com.technews.technewsjavaapi;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int num1 = 1000;
+        double num2 = 24.99;
+        String num3 = "nate";
+
+        double sum = num1 + num2;
+
+        String concat = num1 + num3;
+
+        System.out.println(sum);
+
+        System.out.println(concat);
+    }
+}
