d6a8fb71c6075424aef4e46fc3e5fdd4916fc509
Nate Hanisch <nathan.hanisch86@gmail.com>
2023-04-15 19:36:51 -0400

    added dependency to pom.xml

--- src/main/java/com/technews/technewsjavaapi/controller/PostController.java
+++ src/main/java/com/technews/technewsjavaapi/controller/PostController.java
@@ -6,11 +6,11 @@ import com.technews.technewsjavaapi.model.Vote;
 import com.technews.technewsjavaapi.repository.PostRepository;
 import com.technews.technewsjavaapi.repository.UserRepository;
 import com.technews.technewsjavaapi.repository.VoteRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
